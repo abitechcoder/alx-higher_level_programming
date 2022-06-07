51248182756cba62910643b6731d981b260287d0
abitechcoder <abiolaolalekan39@gmail.com>
2022-06-07 15:45:06 -0700

    updated task 3 and task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,5 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if 0 <= idx < len(my_list):
-        new_list[idx] = element
-    
+        new_list[idx] = element    
     return new_list
