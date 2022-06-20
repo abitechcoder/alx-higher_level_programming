406d936d539c71ff1ebfcea144e125d9823b77b8
abitechcoder <abiolaolalekan39@gmail.com>
2022-06-20 13:24:07 -0700

    Changes made

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             nprinted += 1
         print("")
-    except:
+    except IndexError:
         print("")
         pass
     return nprinted
