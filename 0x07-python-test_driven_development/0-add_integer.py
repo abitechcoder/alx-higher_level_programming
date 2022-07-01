00ac6d7ee0be8a7bc496e5ba2e6b748f6adc3e5d
abitechcoder <abiolaolalekan39@gmail.com>
2022-07-01 06:14:12 -0700

    Task done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-This is "0-add_integer" module
-
-The 0-add_integer module supplies one function, add_integer().
+"""The 0-add_integer module supplies one function, add_integer().
 For example,
 >>> add_integer(1, 2)
 3
 """
+
+
 def add_integer(a, b=98):
-    """
-    Retun the addition of a and b.
+    """Retun the addition of a and b.
     a and b must be integers or floats.
     """
     if type(a) is not int and type(a) is not float:
