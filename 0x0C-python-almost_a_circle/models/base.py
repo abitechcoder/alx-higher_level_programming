5acf2d38430af1e08a4b98034f1146ac1fbc65a1
abitechcoder <abiolaolalekan39@gmail.com>
2022-07-14 07:29:49 -0700

    Task done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 class Base:
+    """
+    A class representing Base class
+
+    Attributes
+    ----------
+    __nb_objects : int
+        The private instance attribute
+    """
+    
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Parameters
+        ----------
+        id : int
+            The argument value passed to init method
+        """
         if id is not None:
             self.id = id
         else:
